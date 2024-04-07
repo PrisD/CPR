56d237436f420dee64f77aa3580937b9080d7a49
PrisD <prisciladellavecchia@gmail.com>
2024-04-07 14:05:39 -0300

    fondo imagen solo Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'pm-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APM-New';
+  showBackgroundImage: boolean = false;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showBackgroundImage = (event.urlAfterRedirects === '/');
+      }
+    });
+  }
+
+  onRouterOutletActivate(component: any) {
+    // Aquí puedes realizar alguna lógica adicional si es necesario
+  }
 }
+
+
