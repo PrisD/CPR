bd22a8f14ea9d5f3c2a1a23ab240ccc77e4a5884
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-04-19 18:46:52 -0300

    psychology

---
+++ src/app/screens/psychology-screens/what-is-mindfulness/what-is-mindfulness.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-what-is-mindfulness',
+  templateUrl: './what-is-mindfulness.component.html',
+  styleUrl: './what-is-mindfulness.component.css'
+})
+export class WhatIsMindfulnessComponent {
+
+}
