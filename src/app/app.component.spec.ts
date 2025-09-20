0bbbf1e0cd1833c610ab4ae6a97f45da69370ecc
PrisD <prisciladellavecchia@gmail.com>
2025-09-20 14:09:45 -0300

    version ok backup 👍

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'APM-New'`, () => {
+  it(`should have as title 'cpr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('APM-New');
+    expect(app.title).toEqual('cpr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('APM-New app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cpr app is running!');
   });
 });
